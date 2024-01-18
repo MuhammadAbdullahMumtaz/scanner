7add0f290c77f01f69cbc62d1670d6c1722e8596
MuhammadAbdullahMumtaz <abdullahmumtaz.fsd@gmail.com>
2024-01-18 17:00:07 +0500

    New file path.js added

---
+++ path.js
@@ -0,0 +1,10 @@
+const html5Qrcode = new Html5Qrcode('reader');
+const qrCodeSuccessCallback = (decodedText, decodedResult)=>{
+    if(decodedText){
+        document.getElementById('show').style.display = 'block';
+        document.getElementById('result').textContent = decodedText;
+        html5Qrcode.stop();
+    }
+}
+const config = {fps:10, qrbox:{width:250, height:250}}
+html5Qrcode.start({facingMode:"environment"}, config,qrCodeSuccessCallback );
